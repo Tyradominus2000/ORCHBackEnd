15001663e361046df7977e4cc4174d472d568394
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-03-24 14:50:08 +0100

    Test commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyparser = require("body-parser");
 const express = require("express");
 const mysql = require("mysql");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const http = require("http");
@@ -27,6 +28,28 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post("/AddRegister", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const sql = `SELECT * FROM login WHERE email="${email}"`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result.length === 0) {
+      const sql = ` INSERT INTO login (username, email, password) VALUES ( ?, ?, ?)`;
+      const values = [username, email, password];
+
+      connection.query(sql, values, (err, result) => {
+        if (err) throw err;
+        console.log("Utilisateur ajouté à la base de données");
+        res.send(JSON.stringify(true));
+      });
+    } else {
+      console.log("Utilisateur existant");
+      res.send(JSON.stringify(false));
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server Node écoutant sur le port ${port}`);
