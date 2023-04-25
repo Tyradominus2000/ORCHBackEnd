3a0f204f403e00625bbc60a6ad9663fdbdbf3c5a
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-04-25 11:22:29 +0200

    feat !: added condition to not add a new user if email already in use

--- routes/apis/AddUser.js
+++ routes/apis/AddUser.js
@@ -9,17 +9,26 @@ router.post("/", (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  //hash the password
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    // Store hash in your password DB.
+
+  const sql = `SELECT * FROM users WHERE Useremail = "${email}"`;
+  connection.query(sql, (err, result) => {
     if (err) throw err;
-    const sql = `INSERT INTO users (Username, Useremail, Userpassword) VALUES ( ?, ?, ?)`;
-    const values = [username, email, hash];
-    connection.query(sql, values, (err, result) => {
-      if (err) throw err;
-      console.log("Utilisateur ajouté à la base de données");
-      res.send(JSON.stringify(true));
-    });
+    if (result.length === 0) {
+      //hash the password
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        if (err) throw err;
+        const sql = `INSERT INTO users (Username, Useremail, Userpassword) VALUES ( ?, ?, ?)`;
+        const values = [username, email, hash];
+        connection.query(sql, values, (err, result) => {
+          if (err) throw err;
+          console.log("Utilisateur ajouté à la base de données");
+          res.send(JSON.stringify(true));
+        });
+      });
+    } else {
+      res.send(JSON.stringify(false));
+    }
   });
 });
 
