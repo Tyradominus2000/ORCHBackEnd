a0b20f52a1b339f38cf75ef7151c48f92454d3bf
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-04-25 09:57:05 +0200

    fix : change in domain I HATE COOKIE

--- routes/apis/Logout.js
+++ routes/apis/Logout.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.delete("/", (req, res) => {
   res.clearCookie("token", {
-    domain: "backend-zuaq.onrender.com",
+    domain: ".backend-zuaq.onrender.com",
     path: "/",
     httpOnly: false,
     sameSite: "none",
