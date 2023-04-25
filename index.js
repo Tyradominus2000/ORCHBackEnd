631f0c20c22ebd110a2251323e77613a48125abf
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-04-25 09:38:42 +0200

    fix : allow origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ connection.connect((err) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
